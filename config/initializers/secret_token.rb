c0269ba553d686a0bcea3bebe321313c1284c0e7
Your Name <lafayette.seller@gmail.com>
2012-04-24 22:23:37 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sequential::Application.config.secret_token = '97d2142eac176d57fd16dd99b0e6d6401f3f4f0598e434644bb8c2a1e9e02a82ee37fade29e1cd79684be909b59ada0bb279dcfd45f74ea10d3ef2be122d9f99'
