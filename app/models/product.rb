bfb60b119789982ae49655f15ed0acb75dcda001
Your Name <lafayette.seller@gmail.com>
2012-04-25 21:08:59 -0500

    Top O 9.3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
+  
   validates :title, presence: true, uniqueness: true
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)$}i, message: 'must be a URL for GIF, JPG, or PNG image.'} 
   validates :description, :image_url, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Sequential::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
