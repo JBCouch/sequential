1e61f9b05f5e548610de53c76f0f13154a4f81d6
Your Name <lafayette.seller@gmail.com>
2012-04-27 00:53:39 -0500

    Beginning of Section 11.2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,15 +42,12 @@ class LineItemsController < ApplicationController
   def create 
   
     @cart = current_cart
-    product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product_id: product.id, cart_id: @cart_id)
+    @line_item = @cart.add_product(params[:product_id])
 
     respond_to do |format|
   
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
-      
+        format.html { redirect_to store_path }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
